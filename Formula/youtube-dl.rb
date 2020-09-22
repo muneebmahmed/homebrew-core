ad797982c9e1759dcc84c3a131d38b78ef59ca46
Muneeb Ahmed <muneebmahmed@gmail.com>
2020-09-21 18:05:56 -0700

    youtube-dl: Add python 3 dependency

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -12,8 +12,13 @@ class YoutubeDl < Formula
 
   bottle :unneeded
 
+  depends_on "python@3.8" if MacOS.version <= :mojave
+
   def install
-    system "make", "PREFIX=#{prefix}" if build.head?
+    # Use `python3` instead of the unversioned `python`, which may point to a
+    # Python 2 installation.
+    system "make", "PYTHON=/usr/bin/env python3", "PREFIX=#{prefix}" if build.head?
+    inreplace "youtube-dl", %r{^#!/usr/bin/env python$}, "#!/usr/bin/env python3" unless build.head?
     bin.install "youtube-dl"
     man1.install "youtube-dl.1"
     bash_completion.install "youtube-dl.bash-completion"
